4952f9295c5a2dca166e6dab424fa233d5ec728c
GXtal <zazaza00133@gmail.com>
2023-02-18 20:26:30 +0300

    trying to understand how to work

--- lab-express-ejs/app.js
+++ lab-express-ejs/app.js
@@ -3,4 +3,4 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.listen(port, () => console.log('listnning on port ${port}'))
+app.listen(port, () => console.log(`listnning on port ${port}`));
